1ce1241eb25121a7d80d61037b9a039067b8cba2
Adam Craig and Ashley Maceli <adam@>
2016-04-26 10:19:51 -0700

    add passing JUnit test for the foodPasses method decreasing foodLevel properly

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -4,12 +4,14 @@ public class Tamagotchi {
   private int mFoodLevel;
   private int mSleepLevel;
   private int mActivityLevel;
+  private int mPoopLevel;
 
   public Tamagotchi(String name) {
     mName = name;
     mFoodLevel = 10;
     mSleepLevel = 10;
     mActivityLevel = 10;
+    mPoopLevel = 0;
   }
 
   public String getName() {
@@ -26,5 +28,12 @@ public class Tamagotchi {
 
   public int getActivityLevel() {
     return mActivityLevel;
+  }
+
+  public void timePasses() {
+    mFoodLevel -= 1;
+    mSleepLevel -= 1;
+    mActivityLevel -= 1;
+    mPoopLevel += 1;
   }
 }
--- src/test/java/TamagotchiTest.java
+++ src/test/java/TamagotchiTest.java
@@ -11,4 +11,11 @@ public class TamagotchiTest {
     assertEquals(10, myPet.getSleepLevel());
     assertEquals(10, myPet.getActivityLevel());
   }
+
+  @Test
+  public void timePasses_foodDecreasesByOne_9() {
+    Tamagotchi myPet = new Tamagotchi("little whale");
+    myPet.timePasses();
+    assertEquals(9, myPet.getFoodLevel());
+  }
 }
